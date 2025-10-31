014e8f9bc79aba7dff2862c4c7d5ea23c1169590
AGBEVE Ayao Nicodème <nicdeme.agbeve@gmail.com>
2025-10-31 23:30:02 +0000

    revoobit ini

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,3 +1,16 @@
 export const dynamic = 'force-dynamic';
 
-// ...existing code...
+export default function ForgotPasswordPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  return (
+    <div>
+      <h1>Forgot Password</h1>
+      <p>
+        A form to request a password reset link will be displayed here.
+      </p>
+    </div>
+  );
+}
