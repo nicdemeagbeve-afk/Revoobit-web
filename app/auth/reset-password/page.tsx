cc971a423656a430c81954a41809d8da727134ca
AGBEVE Ayao Nicodème <nicdeme.agbeve@gmail.com>
2025-10-31 23:26:51 +0000

    revoobit ini

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,3 +1,17 @@
 export const dynamic = 'force-dynamic';
 
-// ...existing code...
+export default function ResetPasswordPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  return (
+    <div>
+      <h1>Reset Password</h1>
+      <p>
+        A form to reset the user's password will be displayed here.
+      </p>
+      {searchParams.code && <p>Code: {searchParams.code}</p>}
+    </div>
+  );
+}
